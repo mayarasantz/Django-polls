95369ab9818e3a7f12a364138c73ce56ad86b9e0
Mayara Timoteo <mayasantoss1409@gmail.com>
2023-09-27 08:22:48 -0300

    UPDATE - CRUD Básico.

--- polls/urls.py
+++ polls/urls.py
@@ -4,12 +4,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('', views.index, name='index'),
-path('sobre/', views.sobre, name='sobre'),
-path('pergunta/', views.exibe_questao, name='exibe_questao'),
-path('perguntas', views.ultimas_perguntas, name='ultimas_perguntas'),
-path('perguntas/list', views.ultimas_perguntas, name='polls_list'),
-path('pergunta/add', views.QuestionCreateView.as_view(), name="poll_add"),
+    path('', views.index, name='index'),
+    path('sobre/', views.sobre, name='sobre'),
+    path('pergunta/', views.exibe_questao, name='exibe_questao'),
+    path('perguntas', views.ultimas_perguntas, name='ultimas_perguntas'),
+    path('perguntas/list', views.ultimas_perguntas, name='polls_list'),
+    path('pergunta/add', views.QuestionCreateView.as_view(), name="poll_add"),
+
+    path('cadastrar',
+        views.QuestionCreateView.as_view(),
+        name = 'question-create'
+    ),
+
+    path('pergunta/<int:pk>/edit',
+        views.QuestionUpdateView.as_view(),
+        name="poll_edit"
+    ),
 
 
 ]
--- polls/views.py
+++ polls/views.py
@@ -27,6 +27,8 @@ def ultimas_perguntas(request):
     return render(request, 'perguntas_recentes.html', context)
 
 
+
+
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 
@@ -35,3 +37,24 @@ class QuestionCreateView(CreateView):
     template_name = 'polls/question_form.html'
     fields = ('question_text', 'pub_date', )
     success_url = reverse_lazy('polls_list')
+
+def get_context_data(self, **kwargs):
+    context = super(QuestionCreateView, self).get_context_data(**kwargs)
+    context['form_title'] = 'Criando uma pergunta'
+
+    return context
+
+
+from django.views.generic.edit import CreateView, UpdateView
+
+class QuestionUpdateView(UpdateView):
+    model = Question
+    template_name = 'polls/question_form.html'
+    fields = ('question_text', 'pub_date', )
+    success_url = reverse_lazy('polls_list')
+
+def get_context_data(self, **kwargs):
+    context = super(QuestionUpdateView, self).get_context_data(**kwargs)
+    context['form_title'] = 'Editando a pergunta'
+    
+    return context
